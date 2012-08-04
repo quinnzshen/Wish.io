8056ca2929dd8c7cc0fcc0adddace9466a628479
Quinn Shen <qzshen@eecs.berkeley.edu>
2012-08-04 06:52:38 +0000

    added things into views.py 'errors'

--- wishio/wishlist/views.py
+++ wishio/wishlist/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("You're at the home page!")
+
+def register(request):
+    return HttpResponse("You're at the register page!")
+
+def mywishlist(request):
+    return HttpResponse("You're at mywishlist!")
+
