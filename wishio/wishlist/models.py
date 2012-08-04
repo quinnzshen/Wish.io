2cfc2e07321ebec5c1e0d42ff84add898b039312
Elizabeth Lin <elizabethylin@gmail.com>
2012-08-03 22:05:31 -0700

    New edits...

--- wishio/wishlist/models.py
+++ wishio/wishlist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=21, unique=True)
+    password = models.CharField(max_length=200)
+    friends = models.ManyToManyField('User', null=True, blank=True)
+
+class Item(models.Model):
+    url = models.URLField()
+    price = models.DecimalField(decimal_places=2)
