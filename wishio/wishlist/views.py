4e31ce4b7fdc494fc5b3b4f3644560a3b26c1773
Quinn Shen <qzshen@eecs.berkeley.edu>
2012-08-04 08:53:39 +0000

    added views for the corresponding urls

--- wishio/wishlist/views.py
+++ wishio/wishlist/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from wishlist.models import
 
 def home(request):
     return HttpResponse("You're at the home page!")
@@ -6,5 +7,8 @@ def home(request):
 def register(request):
     return HttpResponse("You're at the register page!")
 
-def mywishlist(request):
-    return HttpResponse("You're at mywishlist!")
+def wishlist(request, board_id = '1'):
+    return HttpResponse("You're at uid = %s" % board_id)
+
+def friends(request):
+    return HttpResponse("You're at the friend page!")
