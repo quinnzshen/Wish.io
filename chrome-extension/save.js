a2b982b44cb8687f45d160c8c9a176146b807567
Quinn Z Shen <quinn.z.shen@gmail.com>
2012-08-04 13:31:24 -0700

    made chrome extension redirect you to a new page.

--- chrome-extension/save.js
+++ chrome-extension/save.js
@@ -3,5 +3,9 @@ function windowLoaded() {
   chrome.tabs.getSelected(null, function(tab) {
     document.getElementById('currentLink').innerHTML = tab.url;
     console.log(tab.url);
+    chrome.tabs.create({
+    	selected: true,
+    	url: "http://127.0.0.1:8000"
+    })
   });
 }
