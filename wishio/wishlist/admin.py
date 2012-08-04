f5eb12e382d6c29c555fa5a3b38feb1b78ddc1bb
Elizabeth Lin <elizabethylin@gmail.com>
2012-08-03 23:04:43 -0700

    Edited admin page

--- wishio/wishlist/admin.py
+++ wishio/wishlist/admin.py
@@ -1,6 +1,14 @@
 from wishlist.models import User, Item, WishList
 from django.contrib import admin
 
+
+class ItemsInline(admin.TabularInline):
+    model = Item
+
+class WishListAdmin(admin.ModelAdmin):
+    fields = ['name', 'privacy', 'owner', 'members']
+    inlines = [ItemsInline]
+
 admin.site.register(User)
 admin.site.register(Item)
-admin.site.register(WishList)
+admin.site.register(WishList, WishListAdmin)
