0a5c94503ce67eee374454da5e899db21cceccc7
Elizabeth Lin <elizabethylin@gmail.com>
2012-08-04 07:24:59 -0700

    IEJSRIEJ

--- wishio/wishlist/models.py
+++ wishio/wishlist/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
     name = models.CharField(max_length=21, unique=True)
     password = models.CharField(max_length=200)
     friends = models.ManyToManyField('User', null=True, blank=True)
+    wishlist = models.ForeignKey('WishList', null=True, blank=True)
 
     def __unicode__(self):
         return self.name
@@ -19,7 +20,7 @@ class Item(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField()
     price = models.DecimalField(max_digits=9, decimal_places=2)
-    rank = models.IntegerField(unique=True)
+    #rank = models.IntegerField(unique=True)
     wishlist = models.ForeignKey('WishList')
 
     def __unicode__(self):
@@ -29,8 +30,8 @@ class Item(models.Model):
 class WishList(models.Model):
     name = models.CharField(max_length=200)
     privacy = models.CharField(max_length=10, choices=privacies)
-    owner = models.ForeignKey('User', related_name="owner")
-    members = models.ForeignKey('User', related_name="members")
+    owner = models.ForeignKey('User', related_name="owner", null=True, blank=True)
+    members = models.ForeignKey('User', related_name="members", null=True, blank=True)
     items = models.ForeignKey('Item', related_name="items", null=True, blank=True)
 
     def __unicode__(self):
--- wishio/wishlist/views.py
+++ wishio/wishlist/views.py
@@ -1,14 +1,89 @@
-from django.http import HttpResponse
-from wishlist.models import
+from django.template import Context
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from wishlist.models import User, Item, WishList
+from urllib2 import urlopen
+from django.core.exceptions import ObjectDoesNotExist
 
 def home(request):
-    return HttpResponse("You're at the home page!")
+    if request.method == 'POST':
+        try: 
+            user = User.objects.get(name=request.POST['user'])
+            if user.password == request.POST['password']:
+                request.session['user'] = user
+                url = '/wishlist/' + str(user.wishlist.pk)
+                return HttpResponseRedirect(url)
+        except:
+            return HttpResponseRedirect('/register/')
+
+    con = Context()
+    con.update(csrf(request))
+    return render_to_response('index.html', con)
 
 def register(request):
-    return HttpResponse("You're at the register page!")
+    con = Context()
+
+    if "register" in request.POST:
+        # Try clause checks if all fields are filled out.
+        try:
+            username = request.POST['user']
+            password = request.POST['password']
+        except ObjectDoesNotExist:
+            con['empty_fields'] = True
+            con.update(crsf(request))
+            return render_to_response('register.html', con)
+
+        new_user = User.objects.create(
+            name=username,
+            password=password
+        )
+
+        # Creates a default public wishlist for the new user
+        wishlist_name = username  + "'s Wishlist"
+        new_user.wishlist = WishList.objects.create(
+            name=wishlist_name,
+            privacy='public',
+            owner=new_user,
+        )
+
+        new_user.save()
+
+        request.method = None
+
+        # Redirects user to the log in page.
+        return HttpResponseRedirect('/')
+
+    con.update(csrf(request))
+    return render_to_response('register.html', con)
+
 
 def wishlist(request, board_id = '1'):
-    return HttpResponse("You're at uid = %s" % board_id)
+    # if 'user' not in request.session:
+    #     return HttpResponseRedirect('/')
+
+    wishlist = WishList.objects.get(pk=board_id)
+
+    def add_item(url):
+        response = urlopen(url)
+        html = response.read()
+        #script to retrieve
+        item = Item(name="Dress", url=url, price=1000.00)
+        item.wishlist = wishlist
+        item.save()
+
+    add_item("http://www.google.com")
+    add_item("http://www.google.com")
+    add_item("http://www.google.com")
+    add_item("http://www.google.com")
+
+    items = [item for item in Item.objects.filter(wishlist=wishlist)]
+    
+    con = Context({
+            'wishlist': wishlist,
+        })
+    return render_to_response('wishlist.html', con)
+
 
 def friends(request):
     return HttpResponse("You're at the friend page!")
